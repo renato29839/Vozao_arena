39ec4bfb00f966be90119f7e6594b587b4ec19cc
renato29839 <renatogabrielmv@gmail.com>
2026-02-02 14:20:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -90,15 +90,7 @@ async function confirmarAgendamento() {
         });
 
         const msg = `Vozão! Reserva Arena Fábrica de Craques.
-        Informações gerais:
-        - Valores: 
-        R$ 150,00 – 1 hora de jogo
-        R$ 225,00 – 1 hora e meia de jogo
-
-        - Disponibilizamos bolas para uso durante a partida.
-
-        Endereço:
-        Rua Júlio César, 445 – Jardim América
+        Informações gerais: %0A- Valores: %0AR$ 150,00 – 1 hora de jogo %0A R$ 225,00 – 1 hora e meia de jogo%0A- Disponibilizamos bolas para uso durante a partida.%0AEndereço:%0ARua Júlio César, 445 – Jardim América
         %0A*Campo:* ${campo.replace('_',' ')}%0A*Atleta:* ${nome.toUpperCase()}%0A*Data:* ${data.split('-').reverse().join('/')}%0A*Hora:* ${horarioSelecionado}%0A*Pix:* ${PIX_INFO}`;
         window.open(`https://api.whatsapp.com/send?phone=${WHATSAPP_RESPONSAVEL}&text=${msg}`, '_blank');
         
@@ -139,3 +131,4 @@ seletorData.addEventListener('change', renderizarGrade);
 seletorCampo.addEventListener('change', renderizarGrade);
 
 carregarDados();
+
